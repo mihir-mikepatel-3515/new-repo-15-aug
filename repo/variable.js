cbe6e1120abf3d1de0eaa588867a0f43d5f94402
mihir-mikepatel-3515 <mihir3515@gmail.com>
2026-09-15 13:19:50 +0530

    new change

---
+++ repo/variable.js
@@ -0,0 +1,24 @@
+const pie=3.14;
+let person ="mihir";
+let education = "btech";
+let  job = "developer";
+let newJob ;
+
+dream = "to become a successful software engineer";
+
+
+// pie=3.15;
+person = "mihir patel";
+education = "btech+MS";
+job = "developer+buisness analysist";
+dream = "to become a successful software engineer and buisness analysist";  
+
+
+// console.log(pie);
+// console.log(person);
+// console.log(education);
+// console.log(job);
+// console.log(dream);
+
+// console.table({pie,person,education,job,dream});
+console.table([pie,person,education,job,dream,newJob]);  
