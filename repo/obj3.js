5d2b89a1add5ec073d539fd48433f9ce4f676613
mihir-mikepatel-3515 <mihir3515@gmail.com>
2026-09-15 16:15:09 +0530

    some complete

---
+++ repo/obj3.js
@@ -0,0 +1,14 @@
+const course = {
+    courseName: "JavaScript", 
+    price: 299, 
+    courseInstructor: "Mike"};
+// console.log(course.courseName);
+// console.log(course["price"]);
+// const{courseInstructor : teacher} = course;
+console.log(teacher);
+
+// {
+//     "name": "Mikepatel",
+//     "price": "299",
+//     "courseInstructor": "Mike"    
+// }
