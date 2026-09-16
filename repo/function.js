60a9f339e428f0867b26de9fd195be9c31b8ecaa
mihir-mikepatel-3515 <mihir3515@gmail.com>
2026-09-16 10:21:43 +0530

    function

---
+++ repo/function.js
@@ -0,0 +1,65 @@
+// // function sayMyName(){
+// //     console.log("My name is John Doe");
+// //     console.log("I am a software developer");
+// //     console.log("I love coding in JavaScript");
+// //     console.log("I enjoy solving problems and building applications");
+// // }
+
+// // // sayMyName();
+
+// // // function addNumbers(a, b){
+// // //     // return a + b;
+// // //     console.log(a + b); 
+// // // }
+// // function addNumbers(a, b){
+// //     return a + b;
+  
+// // }
+
+// // const result = addNumbers(3,7);
+// // // console.log("result: ", result); // undefined 
+
+// // // console.log(addNumbers(5, 10));
+// // // function loginUserMessage(username){
+// // //     if(!username){
+// // //         console.log("Username is required");
+// // //         return;
+// // //     }
+// // //     return `${username} just logged in`;
+// // // }
+// // // // console.log(loginUserMessage(""));
+// //  // Mike just logged in
+
+
+
+// // function calculateTotalPrice(v1,v2,...num1){
+// //     return num1
+// // }
+// // console.log(calculateTotalPrice(100,200,300,400,500,600,700,800,900,1000)); 
+
+
+// // const user = {
+// //     userName: "Mikepatel",
+// //     price: 299
+// // }
+
+// function handleObject(obj){{
+//     console.log(`Username is ${obj.userName} and price is ${obj.price}`);
+
+// }
+// }
+
+// // handleObject(user);
+// handleObject({
+//     userName: "Mikepatel",
+//     price: 299
+// }); 
+
+
+const myNewArray = [1,2,3,4,5,6,7,8,9,10];
+
+function returnSecondVlaue(getarray){
+    return getarray[1];
+}
+console.log(returnSecondVlaue([200,300,400,500])); // 3
+   
