90f156634ba580521c5a21eb811030ad6da2928e
mihir-mikepatel-3515 <mihir3515@gmail.com>
2026-09-16 13:30:20 +0530

    dd

---
+++ repo/loops.js
@@ -0,0 +1,22 @@
+
+
+
+// // // 
+// let myArray =["batman","superman", "hitman"]
+// console.log(myArray.length);
+// for (let index =0; index<myArray.length;index++)
+// {
+//     const element =myArray[index];
+//     console.log(element);
+// }
+for(let index=1;index<=20;index++)
+{
+    if(index==5)
+    {
+        console.log("detect 5");
+        continue;
+
+    }
+
+    console.log(`value is ${index}`)
+}
