aa16e2ba796caba6880294fd413faaea1ddc9b3d
mihir-mikepatel-3515 <mihir3515@gmail.com>
2026-09-16 10:55:38 +0530

    closer

---
+++ repo/closer.js
@@ -0,0 +1,30 @@
+// function one(){
+//     const userName="Mike";
+
+//     function two (){
+//         const website ="youtube";
+//         console.log(userName);
+//     }
+//     // console.log(website); 
+//     two();
+// }
+// one();
+// if (true) {
+//     const userName = "Mike";
+//     if (userName === "Mike") {
+//         const website = " youtube";
+//         console.log(userName+ website); // Mike youtube
+//     }
+//     // console.log(website); // ReferenceError: website is not defined
+// }
+// console.log(userName); // ReferenceError: userName is not defined
+
+
+function addone(num)
+
+{
+return num + 1;
+}
+addone(5); // 6
+
+// console.log(addone(5)); // 6
