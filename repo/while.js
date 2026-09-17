08afda9f8815a07b1f55634652b2cc5ec03a44a7
mihir-mikepatel-3515 <mihir3515@gmail.com>
2026-09-17 12:04:23 +0530

    17

---
+++ repo/while.js
@@ -0,0 +1,13 @@
+// 
+const myNum = [1,2,3,4,5,6,7,8,9,10];
+// const newNums = myNum.map((num)=>{return num + 10})
+// const newNums = myNum
+// .map((num)=>num*10)
+// .map((num)=>num+1)
+// .filter((num)=>num>=40)
+// console.log(newNums);
+const newNum= myNum.reduce(function(acc,currval){
+    console.log(`acc:${acc} and currval:${currval}`)
+    return acc + currval
+},0)
+console.log(newNum);
